01b1f99c7175a0cc5f8fe0624453952ae6b0a977
Sohyunnnn <thgus05061@naver.com>
2023-11-24 13:17:56 +0900

    feat: 캐러셀 기능 추가

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,35 +1,25 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import './Carousel.css';
+import { Navigation } from 'swiper/modules';
 
-const Carousel = () => {
-  return (
-    <Swiper
-      spaceBetween={18.34}
-      slidesPerView={5}
-      pagination={{ clickable: true }}
-    >
-      {/* 각 슬라이드 */}
-      <SwiperSlide className='carouselStyle' >
-        <h3>슬라이드 1</h3>
-        {/* 슬라이드 1에 표시될 내용을 추가하세요. */}
-      </SwiperSlide>
-      <SwiperSlide className='carouselStyle'>
-        <h3>슬라이드 2</h3>
-      </SwiperSlide>
-      <SwiperSlide className='carouselStyle'>
-        <h3>슬라이드 3</h3>
-      </SwiperSlide>
-      <SwiperSlide className='carouselStyle'>
-        <h3>슬라이드 4</h3>
-      </SwiperSlide>
-      <SwiperSlide className='carouselStyle'>
-        <h3>슬라이드 5</h3>
-      </SwiperSlide>
-      <SwiperSlide className='carouselStyle'>
-        <h3>슬라이드 6</h3>
-      </SwiperSlide>
+
+  const Carousel = ({ customClass }) => {
+    return (
+      <Swiper
+        slidesPerView={3}
+        centeredSlides={false}
+        spaceBetween={30}
+        navigation={true}
+        modules={[Navigation]}
+        className={`Swiper ${customClass}`}
+      >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
     </Swiper>
   );
 };
