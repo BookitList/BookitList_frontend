31026247010dc623bf62fc98bb4ad5b7ec936ccc
dah <cdh010126r@gmail.com>
2024-01-08 19:32:12 +0900

    Feat: Home Banner 만듬

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
 import './Header.css';
+import InputSearchIcon from 'img/InputSearchIcon.svg';
 
 const Header = () => {
+    const [searchQuery,setSearchQuery] = useState('');
+
+    const onSearchChangeHandler = (e) => {
+        e.preventDefault();
+        setSearchQuery(e.target.value);
+    };
+    
+    // const onFormSubmitHandler = (e) => {
+    //     e.preventDefault();
+    //     console.log(searchQuery);
+    // };
+
+    // const onLoginHandler = (e) => {
+    //     e.preventDefault();
+    //     console.log('login');
+    // }
+
+    const gotoLoginPage = () => {
+        Navigate(`/Login`);
+    };
+
+    const storedId = localStorage.getItem('id');
+
     return (
         <header className='HeaderContainer'>
-            <p>Bookit List
-            장르
-            피드
-            글쓰기
-            마이페이지
-            검색창
-            로그인버튼</p>
+
+            <div className='HeaderLeft'>
+                <div className='LeftItem'>
+                    <a href='/' style={{textDecoration:'none'}}>
+                        Bookit List
+                    </a>
+                </div>
+                <div className='LeftItem'>
+                    <a href='/' style={{textDecoration:'none'}}>
+                        피드
+                    </a>
+                </div>
+                <div className='LeftItem'>
+                    <a href='/' style={{textDecoration:'none'}}>
+                        글쓰기
+                    </a>
+                </div>
+                <div className='LeftItem'>
+                    <a href='/' style={{textDecoration:'none'}}>
+                        마이페이지
+                    </a>
+                </div>
+            </div>
+
+            <div className='HeaderRight'>
+                <img className='InputSearchIcon' src={InputSearchIcon} alt='SearchIcon'/>
+                <input className='SearchBar' type = 'text' placeholder='책이름을 검색하세요' required={true}
+                value={searchQuery} onChange={onSearchChangeHandler}/>
+
+                <button className='LoginButton' onClick={gotoLoginPage} style={{display:storedId ? 'none' : 'block'}}>
+                    로그인
+                </button>
+                
+            </div>
+
         </header>
     )
 }
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Login.css'
+
+const Footer = () => {
+    return(
+        <div className='LoginPage'>
+        </div>
+    )
+}
+
+export default Footer
