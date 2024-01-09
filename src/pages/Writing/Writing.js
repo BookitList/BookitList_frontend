f5306072156f8dbd6bb6522ea677b7b156aa1f64
Sohyunnnn <thgus05061@naver.com>
2024-01-09 20:26:45 +0900

    Feat: 드롭다운에 따라 한줄요약, 포스트 보이게 기능 구현

--- src/pages/Writing/Writing.js
+++ src/pages/Writing/Writing.js
@@ -1,38 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Writing.css';
 import Header from 'components/Header/Header';
-import Dropdown from 'components/Dropdown/Dropdown';
 import Footer from 'components/Footer/Footer';
-import { useState } from 'react';
+import Dropdown from 'components/Dropdown/Dropdown';
 
 const Writing = () => {
+  const [textValue, setTextValue] = useState("");
+  const [postTitle, setPostTitle] = useState("");
+  const [postContent, setPostContent] = useState("");
+  const [isOneLineWritingVisible, setIsOneLineWritingVisible] = useState(true);
+  const [isPostWritingVisible, setIsPostWritingVisible] = useState(false);
 
-    const [textValue, setTextValue] = useState("");
   const handleSetValue = (e) => {
     setTextValue(e.target.value);
   };
 
+  const handleSetTitle = (e) => {
+    setPostTitle(e.target.value);
+  };
+  
+  const handleSetContent = (e) => {
+    setPostContent(e.target.value);
+  };
+
+  const showOneLineWriting = () => {
+    setIsOneLineWritingVisible(true);
+    setIsPostWritingVisible(false);
+  };
+
+  const showPostWriting = () => {
+    setIsPostWritingVisible(true);
+    setIsOneLineWritingVisible(false);
+  };
+
   return (
     <div className='Writing'>
-        <Header />
-        <div className='WritingContainer'>
-            <div className='SelectedBookContainer'>
-                
-            </div>
-            <div className='Dropdown'>dropdown</div>
-            <textarea className='OneLineTextArea'
+      <Header />
+      <div className='WritingContainer'>
+        <div className='SelectedBookContainer'></div>
+        <Dropdown
+          toggleOneLineWriting={showOneLineWriting}
+          togglePostWriting={showPostWriting}
+        />
+        {isOneLineWritingVisible && (
+          <textarea
+            className='OneLineTextArea'
             value={textValue}
             onChange={(e) => handleSetValue(e)}
-            ></textarea>
-            <div className='ButtonContainer'>
-                <button className='TemporaryStorage'>임시저장</button>
-                <button className='Registration'>등록하기</button>
-            </div>
+          ></textarea>
+        )}
+        {isPostWritingVisible && (
+          <div className='PostWriting'>
+          <textarea
+            className='PostTitle'
+            value={postTitle}
+            onChange={(e) => handleSetTitle(e)}
+            placeholder="제목을 입력하세요"
+          ></textarea>
+          <textarea
+            className='PostContent'
+            value={postContent}
+            onChange={(e) => handleSetContent(e)}
+            placeholder="내용을 입력하세요"
+          ></textarea>
         </div>
-        <Footer />
         
+        )}
+        <div className='ButtonContainer'>
+          <button className='TemporaryStorage'>임시저장</button>
+          <button className='Registration'>등록하기</button>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Writing
+export default Writing;
