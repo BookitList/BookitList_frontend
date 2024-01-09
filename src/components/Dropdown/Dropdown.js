f1b4e0212aed915897a308e1957aa8b6cb4c3dd1
Sohyunnnn <thgus05061@naver.com>
2024-01-09 20:25:06 +0900

    Feat: 드롭다운 기능 추가

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,23 +1,29 @@
 import React, { useState } from 'react';
-import './Dropdown.css'; // 스타일 파일 import
+import './Dropdown.css';
 
-const Dropdown = () => {
+const Dropdown = ({ toggleOneLineWriting, togglePostWriting }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [buttonText, setButtonText] = useState('한줄요약'); // 초기 텍스트 설정
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleItemClick = (callback, text) => () => {
+    setIsOpen(false);
+    setButtonText(text); // 클릭한 항목의 텍스트로 버튼 텍스트 변경
+    callback();
+  };
+
   return (
-    <div className="dropdown">
-      <button className="dropbtn" onClick={toggleDropdown}>
-        한줄 요약
-      </button>
+    <div className='Dropdown'>
+      <div className='Dropdown-button' onClick={toggleDropdown}>
+        {buttonText}
+      </div>
       {isOpen && (
-        <div className="dropdown-content">
-          <a href="#">Option 1</a>
-          <a href="#">Option 2</a>
-          <a href="#">Option 3</a>
+        <div className='Dropdown-content'>
+          <div onClick={handleItemClick(toggleOneLineWriting, '한줄요약')}>한줄요약</div>
+          <div onClick={handleItemClick(togglePostWriting, '포스트')}>포스트</div>
         </div>
       )}
     </div>
