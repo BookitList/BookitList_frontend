84558c4810cc3dd8616289a4980bceac9c24df28
dah <cdh010126r@gmail.com>
2024-02-19 20:16:58 +0900

    Feat. access_token, SearchPage ver01

--- src/pages/Login/LoginHandler.js
+++ src/pages/Login/LoginHandler.js
@@ -1,73 +1,85 @@
-import React,{ useEffect, useState } from 'react';
+import React,{useEffect} from 'react';
 // import { useRecoilState } from "recoil";
 // import { userInfoState } from "../recoilAtoms";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+// import axios from "axios";
+// import api from 'components/api.js';
 
-const LoginHandeler = (props) => {
-  // const [userInfo, setUserInfo] = useRecoilState(userInfoState);
+import Header from 'components/Header/Header.js';
+
+const LoginHandeler = () => {
   const navigate = useNavigate();
   let params = new URL(window.location.href).searchParams;
   let access_token = params.get("accessToken");
-  let refresh_token = params.get("refreshToken");
-  const [accessTokenFetching, setAccessTokenFetching] =useState(false)
+  // let refresh_token = params.get("refreshToken");
+  // const [accessTokenFetching, setAccessTokenFetching] =useState(false)
 
   // console.log("access_token: ", access_token)
   // console.log("refresh_token: ", refresh_token)
+  localStorage.setItem("access_token", access_token);
 
-  const getAccessToken = async () => {
-    if (accessTokenFetching) return; // Return early if fetching
-
-    console.log("getAccessToken 호출");
-
-    try {
-        setAccessTokenFetching(true); // Set fetching to true
-
-        const response = await axios.post(
-            "~~~/api/auth/kakao",
-            {
-                authorizationCode: access_token,
-            },
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization : `Bearer ${access_token}`
-
-                },
-            }
-        )
-        .then((res)=>{
-          console.log("데이터 성공 : ");
-          console.log(res);
-          const { data } = res;
-          const {access_token} = data;
-
-          if (access_token) {
-            console.log(`Bearer ${access_token}`);
-            axios
-            .post(
-              "~~~/api/auth/kakao",
-              {},
-              {
-                headers: {
-                  "Content-Type": "application/json",
-                  Authorization : `Baearer ${access_token}`
-
-              },
-              }
-            )}
-      });
-
-        const accessToken = response.data.accessToken;
-        console.log("accessToken:", accessToken);
-        navigate("/");
-        setAccessTokenFetching(false); // Reset fetching to false
-    } catch (error) {
-        console.error("Error:", error);
-        setAccessTokenFetching(false); // Reset fetching even in case of error
+  useEffect(() => {
+    if (access_token) {
+      navigate("/");
     }
-  };
+  }, [access_token, navigate]);
+  
 
+  // console.log("refresh_token: ", refresh_token)
+
+  // const getAccessToken = async () => {
+  //   if (accessTokenFetching) return; // Return early if fetching
+
+  //   console.log("getAccessToken 호출");
+
+
+  //   try {
+  //       setAccessTokenFetching(true); // Set fetching to true
+
+  //       const response = await api.post( //4가지 전달
+  //           "/auth/kakao",
+  //           {},
+  //           {
+  //               headers: {
+  //                   "Content-Type": "application/x-www-form-urlencoded:charset=utf-8",
+  //                   Authorization : `Bearer ${access_token}`
+
+  //               },
+  //           }
+  //       );
+        
+  //       const access_token = response.data.accessToken;
+  //       console.log("accessToken:", access_token);
+  //       setAccessTokenFetching(false); // Reset fetching to false
+  //       navigate("/");
+        
+  //   } catch (error) {
+  //       console.error("Error:", error);
+  //       setAccessTokenFetching(false); // Reset fetching even in case of error
+  //   }
+  // };
+
+//   .then((res)=>{
+//     console.log("데이터 성공 : ");
+//     console.log(res);
+//     const { data } = res;
+//     const {access_token} = data;
+
+//     if (access_token) {
+//       console.log(`Bearer ${access_token}`);
+//       axios
+//       .post(
+//         "/api/auth/kakao",
+//         {},
+//         {
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization : `Baearer ${access_token}`
+
+//         },
+//         }
+//       )}
+// });
   // const post= async()=>{
   //   if (accessTokenFetching) return; // Return early if fetching
 
@@ -97,18 +109,17 @@ const LoginHandeler = (props) => {
   // }
 
 //   useEffect(() => {
-//     if (access_token && !userInfo.accessToken) {
+//     if (access_token) {
 //         getAccessToken();
 //     }
-// }, [access_token, userInfo]);
+// }, [access_token]);
 
   return (
     <div className="LoginHandeler">
+      <Header access_token={access_token} />
       <div className="notice">
         <p>로그인 중입니다.</p>
         <p>잠시만 기다려주세요.</p>
-        <div className="spinner">
-        </div>
       </div>
     </div>
   );
--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,15 +1,98 @@
 import React from 'react';
+import axios from 'axios';
 import './SearchPage.css';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
 
+import heartGaryIcons from 'img/heartGrayIcon.svg';
+
 const SearchPage = () => {
+//   const[query,setQuery] = useState('채식주의자'); //검색어 변수
+//   const[page,setPage] = useState(1); //페이징 변수
+//   const[last,setLast] = useState(1);
+
+//   const [documents,setDocuments] = useState(null);
+
+//   const callAPI = async() =>{
+//     const url = `https://api.bookitlist.store/books/search?target=title&isbn=${isbn}`;
+//     const config = {
+//       headers:'Authorization: KakaoAK dc8d40f2136deeecad5055925f2695db', //JWT Token 'APIKEY'
+//       params:{query:query,size:5,page:page}
+//   }; 
+//     const result = await axios(url,config);
+//     setDocuments(result.data.documents);
+//     const total = result.data.meta.pageable_count;
+//     setLast(Math.ceil(total/10));
+//   }
+
+//   useEffect(()=>{
+//     callAPI();
+//   },[page])
+
+//   const onSubmit  =(e) => {
+//     e.preventDefault();
+//     callAPI();
+//     setPage(1);
+// }
+
+// if(documents===null){
+//     return <h1>로딩중........</h1>
+// }
+
   return (
     <div className='SearchPage'>
         <Header />
         <div className='SearchPageContainer'>
+
+          {/* {documents.map(d=>( */}
             <div className='BookContainer'>
+              <div className='BookImg'
+              style={{
+              backgroundImage: 'heartGrayIcons', // cover 상태를 배경 이미지로 설정
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',}}>
+                {/* <img src={d.thumbnail ? d.thumbnail:'http://via.placeholder.com/180X260'} alt="" /> */}
+              </div>
+              
+              <div className='BookDescription'>
+                <div className='BookTitle'>
+                  {/* `${title}` */} 책 제목
+                  </div>
+                <img className='heart' src={heartGaryIcons} alt='Heart'></img>
+                
+                <div className='BookDetail'>
+                  <h6>저자</h6>
+                    {/* `${author}` */}
+                  <h6>출판사</h6>
+                    {/* `${publisher}` */}
+                  <h6>출판일</h6>
+                    {/* `${pubDate}` */}
+                  <h6 style={{display:'inline'}}>포스트 | </h6>
+                    {/* `(${numPost})`개 */}
+                  <h6 style={{display:'inline'}}>한줄요약 | </h6>
+                    {/* `(${numOneLine})개` */}
+                </div>
+                <div className='BookIntro'>
+                  <h6>책 소개</h6>
+                    {/* `${description}` */}
+                </div>
+
+                <div className='oneLineContainer'>
+                  <h5> 대표 한줄평 </h5>
+                  <h6> - 코코벤 기록가 </h6>
+                </div>
+              </div>
+
             </div>
+          )
+          {/* ) */}
+          }
+          <div>
+            {/* <button onClick={()=>setPage(page-1)} disabled={page===1}>이전</button>
+            <span style={{margin:'10px'}}>{page}/{last}</span>
+            <button onClick={()=>setPage(page+1)} disabled={page===last}>다음</button> */}
+          </div>
         </div>
         <Footer />
     </div>
